82ba2f93f86ed955ccbf2a8093ecfa91d5baaa05
Javier3javier92 <72465716+Javier3javier92@users.noreply.github.com>
2020-11-24 11:31:49 -0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,12 @@ if(isst($_GET["FIRSTNAME"])){//show feedback
 }else{//show form
   echo '
   <form action="">
-  First Name <input type="text" name="FirstName" />
-  <br />
+ <p>First Name <input type="text" name="FirstName" /></p> 
+ <p>Last Name <input type="text" name="LastName" /></p> 
+ <p>Favorite Color</p>
+ <p><input type="radio" name="FavoriteColor" value="red" /> Red</p>
+ <p><input type="radio" name="FavoriteColor" value="blue" /> Blue</p>
+ <p><input type="radio" name="FavoriteColor" value="yellow" /> Yellow</p>
   <input type="submit">
     
  </form> 
