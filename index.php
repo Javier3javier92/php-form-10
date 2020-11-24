a718bb61fed255c8e2e1f5471b0de6376d9981d4
Javier3javier92 <72465716+Javier3javier92@users.noreply.github.com>
2020-11-24 10:46:36 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,19 @@ echo '<p>This is PHP! </p>';
 //echo '<p>This is PHP!</p>
 
 if(isst($_GET["FIRSTNAME"])){//show feedback
-
+ echo $_GET["FirstName"];
+  
+ echo '<pre>';
+ var_dump($_GET);
+ echo '<pre>';
+  
+  
 }else{//show form
   echo '
   <form action="">
   First Name <input type="text" name="FirstName" />
   <br />
-  <input type=
+  <input type="submit">
     
  </form> 
   ';
